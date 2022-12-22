29402c64c71b96aa7c86a17414c5adcdcb8480ae
Kovács Szilárd <skriemor@gmail.com>
2022-12-22 21:26:21 +0100

    Initial

---
+++ src/main/java/hu/torma/deliveryapplication/DeliveryapplicationApplication.java
@@ -0,0 +1,13 @@
+package hu.torma.deliveryapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/torma/deliveryapplication/DeliveryapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.torma.deliveryapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
